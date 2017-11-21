c15f1a96149366000cfb1e5bdf74b653ea4c010b
Siva Kommuri <snv.kommuri@gmail.com>
2017-11-21 14:51:34 -0800

    [coursera-ml] ex05 - complete 100%

--- coursera-machine-learning/machine-learning-ex5/ex5/validationCurve.m
+++ coursera-machine-learning/machine-learning-ex5/ex5/validationCurve.m
@@ -39,13 +39,39 @@ error_val = zeros(length(lambda_vec), 1);
 %
 %
 
+for i = 1:length(lambda_vec)
+    lambda = lambda_vec(i);
+    % Compute train / val errors when training linear
+    % regression with regularization parameter lambda
+    % You should store the result in error_train(i)
+    % and error_val(i)
 
+    %%%
+    %  REUSE learningcurve.m
+    %%%
 
+    % Compute train/cross validation errors using training examples
+    % X(1:i, :) and y(1:i), storing the result in
+    % error_train(i) and error_val(i)
 
+    ts_x = X; %(1:i, :);
+    ts_y = y; %(1:i);
 
+    m = size(ts_x, 1);
 
+    % Train on the training subset data
+    theta = trainLinearReg([ones(m, 1) ts_x], ts_y, lambda);
 
+    % Training error - error on the training subset data
+    error_train(i) = linearRegCostFunction([ones(m, 1) ts_x], ts_y, theta, 0);
 
+    % Cross Validation error - error on the full cross validation data
+    cv_x = Xval;
+    cv_y = yval;
+    m = size(cv_x, 1);
+    error_val(i) = linearRegCostFunction([ones(m, 1) cv_x], cv_y, theta, 0);
+
+end
 
 
 % =========================================================================
